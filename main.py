bc6e2a59a210c35eecc953c7569f7f1fc0713f4d
cantidev <cantidev1007@gmail.com>
2025-04-01 19:02:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from clientes import Clientes
 cliente = Clientes()
 
 
+#obtenerClienteID = cliente.getClienteById(2)
+
+#eliminarCliente = cliente.deleteClienteById(1)
+
+#cambiarMail = cliente.cambiarCorreo(2, "correocambiado.com")
+
 
 obtenerClientes = cliente.getClientes()
 
